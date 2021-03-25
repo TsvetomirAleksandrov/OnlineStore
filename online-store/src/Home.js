61bb1efda05957a3d8d040b14b11b699530b17f0
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-03-25 14:07:56 +0200

    Importing Product Component

--- online-store/src/Home.js
+++ online-store/src/Home.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './Home.css';
+import Product from './Product';
 
 function Home() {
     return (
-        <div>
+        <div className='home'>
+            <div className='home__container'>
+                <img
+                    className='home__image'
+                    src='http://kupimed.bg/php_assets/uploads/2017/12/1501494_1387167448204622_1184500433_o-boost.jpg'
+                />
 
+                <div className='home__row'>
+                    <Product />
+                    <Product />
+                </div>
+                <div className='home__row'>
+                    {/* Product */}
+                    {/* Product */}
+                    {/* Product */}
+                </div>
+            </div>
         </div>
     );
 };
