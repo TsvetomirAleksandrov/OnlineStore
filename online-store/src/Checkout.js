db71430cf73b778f4860a49f20fbc9e60a0889aa
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-03-25 15:30:09 +0200

    Checkout Component

--- online-store/src/Checkout.js
+++ online-store/src/Checkout.js
@@ -4,7 +4,24 @@ import './Checkout.css';
 function Checkout() {
     return (
         <div className='checkout'>
-            <h1>Hey</h1>
+            <div className='checkout__left'>
+                <img className='checkout__ad' src='http://kupimed.bg/php_assets/uploads/2017/12/1312.jpg'
+                />
+
+                <div>
+                    <h2 className='checkout__title'>Your shopping Basket</h2>
+
+                    {/* Basket Item */}
+                    {/* Basket Item */}
+                    {/* Basket Item */}
+                    {/* Basket Item */}
+                </div>
+            </div>
+
+            <div className='checkout__right'>
+                <h2>The subtotal go here</h2>
+
+            </div>
         </div>
     );
 };
