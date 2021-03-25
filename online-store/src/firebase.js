907c29261ed4c6670a36719330a2d92171fbdefd
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-03-25 11:52:20 +0200

    firebase init

---
+++ online-store/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBi-eGNdjxkMm_515kpuU3Fl0qDYp6ROv0",
+    authDomain: "online-store-dc8f3.firebaseapp.com",
+    projectId: "online-store-dc8f3",
+    storageBucket: "online-store-dc8f3.appspot.com",
+    messagingSenderId: "198389670345",
+    appId: "1:198389670345:web:7833df0797b3b9a4f30818"
+  };
