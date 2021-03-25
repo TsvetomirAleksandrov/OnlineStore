b93d092f9d2ff68d6800e8d96a051afd23bcf11b
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-03-25 15:12:28 +0200

    Importing the Router

--- online-store/src/App.js
+++ online-store/src/App.js
@@ -2,14 +2,27 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-      {/* Footer */}
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path='/checkout'>
+            <h1>I a ma checkout</h1>
+          </Route>
+          <Route path='/'>
+            <Header />
+            <Home />
+          </Route>
+
+        </Switch>
+
+
+        {/* Footer */}
+      </div >
+    </Router>
   );
 }
 
