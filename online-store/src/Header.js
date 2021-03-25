9398c333fff0cd20cc88bc3c1e871bcccef9e9d9
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-03-25 12:33:37 +0200

    Working on the header

---
+++ online-store/src/Header.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    return (
+        <div className='header'>
+            <img className='header__logo'
+                src='http://kupimed.bg/php_assets/uploads/2017/12/Untitled1.jpg-2048x1736.png' />
+
+            <div className='header__search'>
+                <input
+                    className='header__searchInput'
+                    type='text' />
+                {/* Logo */}
+            </div>
+
+            <div className='header__nav'>
+                <div className='header__option'>
+                    <span
+                        className='header__optionLineOne'>Hello Guest
+                        </span>
+                    <span
+                        className='header__optionLineTwo'>Sign In
+                        </span>
+                </div>
+
+                <div className='header__option'>
+                <span
+                        className='header__optionLineOne'>Returns
+                        </span>
+                    <span
+                        className='header__optionLineTwo'>& Orders
+                        </span>
+                </div>
+
+                <div className='header__option'>
+                <span
+                        className='header__optionLineOne'>Your
+                        </span>
+                    <span
+                        className='header__optionLineTwo'>Prime
+                        </span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
