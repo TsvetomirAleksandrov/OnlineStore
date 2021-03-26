34775d994f988fb3fdc3a99a64a027c53aaef722
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-03-26 13:21:49 +0200

    user update

--- online-store/src/reducer.js
+++ online-store/src/reducer.js
@@ -1,5 +1,6 @@
 export const initialState = {
     basket: [],
+    user: null
 };
 
 // Selector
@@ -15,24 +16,24 @@ const reducer = (state, action) => {
             };
 
         case 'REMOVE_FROM_BASKET':
-           const index = state.basket.findIndex(
-               (basketItem) => basketItem.id === action.id
-           );
-           let newBasket = [...state.basket];
+            const index = state.basket.findIndex(
+                (basketItem) => basketItem.id === action.id
+            );
+            let newBasket = [...state.basket];
 
-           if (index >= 0) {
-               newBasket.splice(index, 1);
+            if (index >= 0) {
+                newBasket.splice(index, 1);
 
-           } else {
-               console.warn(
-                   `Can't remove product (id: ${action.id}) as its not in the basket!`
-               );
-           }
+            } else {
+                console.warn(
+                    `Can't remove product (id: ${action.id}) as its not in the basket!`
+                );
+            }
 
-           return {
-               ...state,
-               basket: newBasket
-           }
+            return {
+                ...state,
+                basket: newBasket
+            }
 
         default:
             return state;
