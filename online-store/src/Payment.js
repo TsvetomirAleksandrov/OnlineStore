017cfaebbd1d6a0e8f6e91cbea88647ad2afe196
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-03-26 15:36:04 +0200

    Payment Component

--- online-store/src/Payment.js
+++ online-store/src/Payment.js
@@ -2,6 +2,7 @@ import React from 'react';
 import CheckoutProduct from './CheckoutProduct';
 import './Payment.css';
 import { useStateValue } from './StateProvider';
+import { Link } from 'react-router-dom';
 
 function Payment() {
     const [{ basket, user }, dispatch] = useStateValue();
@@ -9,6 +10,11 @@ function Payment() {
     return (
         <div className='payment'>
             <div className='payment__container'>
+                <h1>
+                    Checkout (<Link to='checkout'>{basket?.length} items
+                    </Link>)
+                </h1>
+
                 <div className='payment__section'>
                     <div className='payment__title'>
                         <h3>Delivery Address</h3>
@@ -42,7 +48,7 @@ function Payment() {
                         <h3>Payment Method</h3>
                     </div>
                     <div className='payment__details'>
-                     //Stripe functionality
+                        //Stripe functionality
                     </div>
                 </div>
             </div>
