d4e69549f4d164470b904a0cf7966ce7ff86011c
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-03-26 12:29:31 +0200

    Define signIn and register func

--- online-store/src/Login.js
+++ online-store/src/Login.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import { Link } from 'react-router-dom';
 
 
 function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = (e) => {
+        e.preventDefault();
+
+    }
+
+    const register = (e) => {
+        e.preventDefault();
+
+        
+    }
+
     return (
         <div className='login'>
             <Link to='/'>
@@ -15,20 +29,20 @@ function Login() {
 
                 <form>
                     <h5>E-mail</h5>
-                    <input type='text' />
+                    <input type='text' value={email} onChange={e => setEmail(e.target.value)} />
 
                     <h5>Password</h5>
-                    <input type='password' />
+                    <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
 
-                    <button className='login__signInButton'>Sign In</button>
+                    <button type='submit' onClick={signIn} className='login__signInButton'>Sign In</button>
                 </form>
 
                 <p>
-                    By signing-in you agree to the Kupimed.bg Conditions of Use & Sale. Please 
+                    By signing-in you agree to the Kupimed.bg Conditions of Use & Sale. Please
                     see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
                 </p>
 
-                <button className='login__registerButton'>Create your Account</button>
+                <button type='submit' onClick={register} className='login__registerButton'>Create your Account</button>
             </div>
         </div>
     );
