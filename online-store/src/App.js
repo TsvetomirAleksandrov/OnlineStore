3c99086d576236c15ab33843c75b6e38ef8da1e0
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-03-29 20:24:19 +0300

    App Component

--- online-store/src/App.js
+++ online-store/src/App.js
@@ -1,66 +1,72 @@
-import React, { useEffect } from 'react';
-import './App.css';
-import Header from './Header';
-import Home from './Home';
-import Login from './Login';
-import Checkout from './Checkout';
-import Payment from './Payment';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { auth } from './firebase';
-import { useStateValue } from './StateProvider';
-import { loadStripe } from '@stripe/stripe-js';
-import { Elements } from '@stripe/react-stripe-js';
+import React, { useEffect } from "react";
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Checkout";
+import Login from "./Login";
+import Payment from "./Payment";
+import Orders from "./Orders";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 
 const promise = loadStripe('pk_test_51IZFxCKyrTYhmZjRBxrvyHk5rq2YRvJMLAvdQTJ2qYOV9fJHDcdmHCBEZiD63vL0KXoZOTGqpQ0oBnHtIVwZrmJt00Wz2DRaog');
 
 function App() {
-  const [{ }, dispatch] = useStateValue();
+  const [{}, dispatch] = useStateValue();
 
   useEffect(() => {
-    auth.onAuthStateChanged(authUser => {
-      console.log('The user is >>>>', authUser);
+    // will only run once when the app component loads...
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
 
       if (authUser) {
-        //the user just logged in / the user is already logged in
+        // the user just logged in / the user was logged in
+
         dispatch({
-          type: 'SET_USER',
-          user: authUser
-        })
+          type: "SET_USER",
+          user: authUser,
+        });
       } else {
-        //the user is logged out
+        // the user is logged out
         dispatch({
-          type: 'SET_USER',
-          user: null
-        })
+          type: "SET_USER",
+          user: null,
+        });
       }
-    })
+    });
   }, []);
 
   return (
     <Router>
       <div className="app">
         <Switch>
-          <Route path='/login'>
+          <Route path="/orders">
+            <Header />
+            <Orders />
+          </Route>
+          <Route path="/login">
             <Login />
           </Route>
-          <Route path='/checkout'>
+          <Route path="/checkout">
             <Header />
             <Checkout />
           </Route>
-          <Route path='/payment'>
+          <Route path="/payment">
             <Header />
             <Elements stripe={promise}>
               <Payment />
             </Elements>
           </Route>
-          <Route path='/'>
+          <Route path="/">
             <Header />
             <Home />
           </Route>
         </Switch>
-
-        {/* Footer */}
-      </div >
+      </div>
     </Router>
   );
 }
