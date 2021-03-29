430c0ad28c9518644afbe31f5d8aae9ee10dc4df
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-03-29 20:23:41 +0300

    Login update

--- online-store/src/Login.js
+++ online-store/src/Login.js
@@ -26,7 +26,6 @@ function Login() {
         auth
             .createUserWithEmailAndPassword(email, password)
             .then((auth) => {
-                console.log(auth);
                 if (auth) {
                     history.push('/');
                 }
