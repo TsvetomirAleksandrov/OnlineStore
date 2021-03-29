123e5c84fd0f4fb5553a0515c8965432269af076
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-03-29 20:22:27 +0300

    Subtotal Completed

--- online-store/src/Subtotal.js
+++ online-store/src/Subtotal.js
@@ -1,39 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import './Subtotal.css';
-import CurrencyFormat from 'react-currency-format';
-import { useStateValue } from './StateProvider';
-import { getBasketTotal } from './reducer';
-import { useHistory } from 'react-router';
-
+import React from "react";
+import "./Subtotal.css";
+import CurrencyFormat from "react-currency-format";
+import { useStateValue } from "./StateProvider";
+import { getBasketTotal } from "./reducer";
+import { useHistory } from "react-router-dom";
 
 function Subtotal() {
-    const history = useHistory();
-    const [{ basket }, dispatch] = useStateValue();
+  const history = useHistory();
+  const [{ basket, user }, dispatch] = useStateValue();
 
-    return (
-        <div className='subtotal'>
-            <CurrencyFormat
-                renderText={(value) => (
-                    <>
-                        <p>
-                            Subtotal ({basket.length} items):
-                     <strong> {value}</strong>
-                        </p>
-                        <small className='subtotal__gift'>
-                            <input type='checkbox' />This order contains a gift
-                 </small>
-                    </>
-                )}
-                decimalScale={2}
-                value={getBasketTotal(basket)}
-                displayType={'text'}
-                thousandSeparator={true}
-                prefix={'$'}
-            />
-
-            <button onClick={e => history.push('/payment')}>Proceed to Checkout</button>
-        </div>
-    );
-};
+  return (
+    <div className="subtotal">
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              {/* Part of the homework */}
+              Subtotal ({basket.length} items): <strong>{value}</strong>
+            </p>
+            <small className="subtotal__gift">
+              <input type="checkbox" /> This order contains a gift
+            </small>
+          </>
+        )}
+        decimalScale={2}
+        value={getBasketTotal(basket)} // Part of the homework
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"$"}
+      />
+      {user ? (<button onClick={e => history.push('/payment')}>Proceed to Checkout</button>) : (<button onClick={e => history.push('/login')}>Proceed to Checkout</button>)}
+    </div>
+  );
+}
 
 export default Subtotal;
