d50293390fc53a229d095ec79fe4656306d3362f
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-04-03 17:48:04 +0300

    App

--- online-store/src/App/App.js
+++ online-store/src/App/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Header from "../Header/Header";
 import Home from "../Home/Home";
@@ -14,29 +14,23 @@ import { Elements } from "@stripe/react-stripe-js";
 import AdminPanel from "../AdminPanel/AdminPanel";
 import Edit from "../AdminPanel/Edit";
 import AddItem from "../AdminPanel/AddItem";
+import { db } from '../firebase';
 import Footer from "../Footer/Footer";
-
 
 const promise = loadStripe('pk_test_51IZFxCKyrTYhmZjRBxrvyHk5rq2YRvJMLAvdQTJ2qYOV9fJHDcdmHCBEZiD63vL0KXoZOTGqpQ0oBnHtIVwZrmJt00Wz2DRaog');
 
 function App() {
-  const [{ }, dispatch] = useStateValue();
+  const [{ user }, dispatch] = useStateValue();
+  const [cartItems, setCartItems] = useState([]);
 
   useEffect(() => {
-    // will only run once when the app component loads...
-
     auth.onAuthStateChanged((authUser) => {
-      console.log("THE USER IS >>> ", authUser);
-
       if (authUser) {
-        // the user just logged in / the user was logged in
-
         dispatch({
           type: "SET_USER",
           user: authUser,
         });
       } else {
-        // the user is logged out
         dispatch({
           type: "SET_USER",
           user: null,
@@ -45,48 +39,86 @@ function App() {
     });
   }, []);
 
+  const getCartItems = () => {
+    db.collection('users')
+      .doc(user?.uid)
+      .collection('cart')
+      .onSnapshot((snapshot) => {
+        const tempItems = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data()
+        }))
+
+        setCartItems(tempItems);
+      })
+  }
+
+  useEffect(() => {
+    getCartItems();
+  })
+
+  const getTotalPrice = () => {
+    let total = 0;
+    cartItems.forEach((item) => {
+      total += (item.item.price * item.item.quantity)
+    })
+
+    return total;
+  }
+
+  const getTotalQuantity = () => {
+    let total = 0;
+    cartItems.forEach((item) => {
+      total += item.item.quantity
+    })
+
+    return total;
+  }
+
   return (
     <Router>
       <div className="app">
+        <Header cartItems={cartItems} />
+
         <Switch>
           <Route path="/add">
-            <Header />
             <AddItem />
-            
           </Route>
+
           <Route path="/edit">
-            <Header />
             <Edit />
-            
           </Route>
+
           <Route path="/admin">
-            <Header />
             <AdminPanel />
-            
           </Route>
+
           <Route path="/orders">
-            <Header />
             <Orders />
-            
           </Route>
+
           <Route path="/login">
             <Login />
           </Route>
+
           <Route path="/checkout">
-            <Header />
-            <Checkout />
-           
+            <Checkout
+              getTotalPrice={getTotalPrice}
+              getTotalQuantity={getTotalQuantity}
+              cartItems={cartItems} />
           </Route>
+
           <Route path="/payment">
-            <Header />
             <Elements stripe={promise}>
-              <Payment />
+              <Payment
+                getTotalPrice={getTotalPrice}
+                getTotalQuantity={getTotalQuantity}
+                cartItems={cartItems} />
             </Elements>
           </Route>
+
           <Route path="/">
-            <Header />
             <Home />
-           
           </Route>
         </Switch>
       </div>
@@ -95,3 +127,4 @@ function App() {
 }
 
 export default App;
+
